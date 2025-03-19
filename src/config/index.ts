0ffd1e9ae39ad7da4a989c38026006d84f3a3e04
samuel <guilherme.engr@gmail.com>
2025-03-19 20:08:19 -0300

    dotenv

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const config = {
     development: {
         BASE_URL: 'http://localhost:3000/v1',
     },
     production: {
-        BASE_URL: 'http://localhost:3000/v1',
+        BASE_URL: process.env.BASE_URL as string,
     },
     projectName: '',
     links: {
